eac433c54f3f2cc90b00125d7c055f86a71282b9
Joshua Tharp <joshua_tharp@yahoo.com>
2014-07-22 00:01:24 -0700

    Working code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,28 @@
 ## Creates a matrix wrapper that can cache its inverse.
 ##
 ## mtrx: an invertable matrix
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mtrx = matrix()) {
+    # The cached inverse
+    invMtrx <- NULL
+
+    set <- function(newMtrx) {
+        mtrx <<- newMtrx
+        invMtrx <<- NULL
+    }
+
+    get <- function() {
+        mtrx
+    }
+
+    setInv <- function(inv) {
+        invMtrx <<- inv
+    }
+
+    getInv <- function() {
+        invMtrx
+    }
+
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 ## This function computes the inverse of the matrix contained within the matrix
@@ -13,4 +34,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ##
 ## cm: a cache matrix object
 cacheSolve <- function(cm, ...) {
+    inv <- cm$getInv()
+    if(!is.null(inv)) {
+        message("Getting cached inverse...")
+    } else {
+        message("Calculating inverse...")
+        mtrx <- cm$get()
+        inv <- solve(mtrx, ...)
+        cm$setInv(inv)
+    }
+
+    inv
 }
